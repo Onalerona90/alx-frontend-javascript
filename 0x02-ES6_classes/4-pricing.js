94dd3892fd834c93d612a85e71fffc113c35656b
Onalerona Pako <onapako90@gmail.com>
2024-07-25 21:26:01 +0200

    EsLint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,4 @@
-import Currency from './3-currency';
-
 export default class Pricing {
-  /* eslint-disable */
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
