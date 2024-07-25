a6bbe96e84a775c665f321abedbc58bc6c4d666e
Onalerona Pako <onapako90@gmail.com>
2024-07-25 20:14:04 +0200

    Use shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,6 @@
 export default function getBudgetObject(income, gdp, capita) {
   /* eslint-disable */
-  const budget = {
-    income,
-    gdp, 
-    capita,
-  };
+  const budget = { income, gdp, capita };
 
   return budget;
 }
