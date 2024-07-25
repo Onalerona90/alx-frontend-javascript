dd5dda64b8caf837cc7451418dd5c6a3216ac30a
Onalerona Pako <onapako90@gmail.com>
2024-07-25 21:09:09 +0200

    Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,8 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   /* eslint-disable */
+  const ClassRoomList = [];
+
   const class1 = new ClassRoom(19);
   const class2 = new ClassRoom(20);
   const class3 = new ClassRoom(34);
